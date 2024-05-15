747df0c30673ef78a91b83beece614fec0cfbf9d
JopetCumpas <142872409+JopetCumpas@users.noreply.github.com>
2024-05-16 07:55:03 +0800

    Update signlanguage.py

--- signlanguage.py
+++ signlanguage.py
@@ -1,25 +1,52 @@
 import streamlit as st
 import tensorflow as tf
-import PIL.Image
 from PIL import Image, ImageOps
 import numpy as np
+import base64
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource
 def load_model():
     model = tf.keras.models.load_model('signlanguage.h5')
     return model
 
 model = load_model()
 
-st.write("""
-# American Sign Language
-""")
+# Function to convert image to base64
+def get_base64_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+
+# Path to your image file
+image_path = 'aslbg.png'
+
+# Generate the base64 image
+base64_image = get_base64_image(image_path)
+
+# CSS to set the background image and adjust file uploader button
+st.markdown(
+    f"""
+    <style>
+    .stApp 
+    {{
+        background: url(data:image/png;base64,{base64_image}) no-repeat center center fixed;
+        background-size: cover;
+    }}
+    .st-emotion-cache-13ln4jf 
+    {{
+        width: 100%;
+        padding: 40rem 1rem 10rem;
+        max-width: 46rem;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 file = st.file_uploader("Choose a hand gesture from the photos", type=["jpg", "png"])
 
 def import_and_predict(image_data, model):
-    size = (64, 64)  # Match the input size with the Google Colab code
-    image = ImageOps.fit(image_data, size, PIL.Image.LANCZOS)  # Use PIL.Image.LANCZOS for resizing
+    size = (50, 50)  # Match the input size with the Google Colab code
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS for resizing
     img = np.asarray(image)
     img = img / 255.0  # Normalize pixel values
     img = np.expand_dims(img, axis=0)
